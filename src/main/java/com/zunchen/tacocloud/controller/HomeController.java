c1b5899803261db11e8a1e6552c926d514842e70
jiangchangyang <930066972@qq.com>
2020-09-02 15:51:39 +0800

    新增登录接口

--- src/main/java/com/zunchen/tacocloud/controller/HomeController.java
+++ src/main/java/com/zunchen/tacocloud/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.zunchen.tacocloud.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @ClassNam HomeController
@@ -15,5 +16,10 @@ public class HomeController {
     @GetMapping("/")
     public String home(){
         return "home";
+    }
+
+    @GetMapping("login")
+    public String login(@RequestParam(value = "") String username, @RequestParam(value = "password") String password){
+        return "";
     }
 }
